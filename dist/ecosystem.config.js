101ba0a7db7b3a11b709752ca4d274b6959d12d1
sunkwolf <sunkwolf@gmail.com>
2024-04-26 17:38:59 -0600

    Create ecosystem.config.js

---
+++ dist/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+      name: "api-ws",
+      script: "./app.js",
+      watch: false,
+      max_memory_restart: "1000M",
+      exec_mode: "cluster",
+      instances: 1,
+      cron_restart: "0 7,12,16 * * *",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }]
+  }
+  
